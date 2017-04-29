904a6ea9ff001d1f45e1fda47772935a1ad5447f
sijezi <mawehelie@gmail.com>
2017-04-29 13:55:14 -0700

    added ejs lib

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var PORT = process.env.port || 3000;
 var express = require('express');
 var app = express();
 
+// routes
+
 app.listen(PORT, function() {
   console.log('app is running on port 3000');
-})
+});
