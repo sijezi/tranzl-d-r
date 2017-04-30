f0daf5f794501f4e56a6a5859275ea9ab2664214
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-29 19:03:26 -0700

    working on authorizing user

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ app.use(passport.session());
 // passport.serializeUser(User.serializeUser());
 // passport.deserializeUser(User.deserializeUser());
 
+
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
 // ROOT ROUTE
 app.get("/", function(req,res){
 	res.render("home");
