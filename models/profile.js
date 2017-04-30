390fe7b2bc312fb9d0c476f5187ee264732e08d7
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-30 14:31:01 -0700

    associating profiles to user

--- models/profile.js
+++ models/profile.js
@@ -2,6 +2,14 @@ var mongoose = require("mongoose");
 var profileSchema = new mongoose.Schema({
 	name: String,
 	availablity: String,
-	languages: String
+	languages: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}
+	}
 
-})
+});
+
+module.exports = mongoose.mode("Campground", camgroundSchema);
